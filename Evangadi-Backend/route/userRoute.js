aad5f4122c69065bfd8048e48c7218b7aae249a0
Lomi-Bedada <lomibedada110215@gmail.com>
2024-12-12 17:54:48 +0400

    file added

--- Evangadi-Backend/route/userRoute.js
+++ Evangadi-Backend/route/userRoute.js
@@ -5,5 +5,7 @@ const authMiddleware = require("../middleWare/authMiddleware");
 
 //user controllers
 const { register, login, checkUser } = require("../controller/userController");
-
+router.post("/register", register);
 router.post("/login", login);
+router.get("/check", authMiddleware, checkUser);
+module.exports = router;
