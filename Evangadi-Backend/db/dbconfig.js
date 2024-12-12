837120ae4960c1395dce92af12bde03e17e84ded
Asiya-Naser <khalidmukemil@gmail.com>
2024-12-12 22:31:05 +0300

    confilct resolve

--- Evangadi-Backend/db/dbconfig.js
+++ Evangadi-Backend/db/dbconfig.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mysql = require('mysql2');
 // const app = express();
 
@@ -67,15 +66,3 @@ module.exports = dbConnection.promise();
 
 
 
-=======
-const mysql = require("mysql2");
-
-const pool = mysql.createPool({
- USER= "u961275249_kebe"
-PASSWORD= "firaol@B123"
-HOST= "82.197.82.42"
-DB_NAME= "u961275249_evangadi_DB"
-});
-
-module.exports = pool.promise();
->>>>>>> Asiya
